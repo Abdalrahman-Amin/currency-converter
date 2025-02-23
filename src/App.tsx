7a57c6d1f91da12de8be15ef4fc80fc673ad778c
Abdalrahman-Amin <abdulrahman.amin@oceanbluesoftware.co.uk>
2025-02-23 04:29:00 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+   const [amount, setAmount] = useState("1");
+   const [convertFrom, setConvertFrom] = useState("USD");
+   const [convertTo, setConvertTo] = useState("EUR");
+   const [convertedAmount, setConvertedAmount] = useState("");
+   useEffect(
+      function () {
+         const controller= new AbortController();
+         async function converCurrency(amount: string, from :string, to:string): Promise<void> {
+            try {
+               if(from === to) {
+                  setConvertedAmount(amount);
+                  return;
+               } 
+               const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${from}&to=${to}`, {signal: controller.signal});
+               const {rates: {[to]: convertedAmount}} = await res.json();
+               setConvertedAmount(convertedAmount);
+               console.log("DEBUG: ~ converCurrency ~ convertedAm:", convertedAmount)
+
+            } catch (error) {
+               console.log("DEBUG: ~ converCurrency ~ error:", error)
+               
+            }
+         }
+         converCurrency(amount, convertFrom, convertTo);
+         return () => controller.abort();
+      },
+      [amount, convertFrom, convertTo]
+   );
+   return (
+      <div className="currency-converter">
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+        className="currency-input"
+        placeholder="Enter amount"
+      />
+      <select
+        value={convertFrom}
+        onChange={(e) => setConvertFrom(e.target.value)}
+        className="currency-select"
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select
+        value={convertTo}
+        onChange={(e) => setConvertTo(e.target.value)}
+        className="currency-select"
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p className="converted-amount">{convertedAmount}</p>
+    </div>
+   );
+}
+
+export default App;
